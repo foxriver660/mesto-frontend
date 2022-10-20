41f10d475db368a0466d1dd97f4c9920636a8aaf
Ivan <foxriver660@gmail.com>
2022-10-20 13:50:46 +0300

    -add Section
    Co-authored-by: BKonstantine <BKonstantine@users.noreply.github.com>

---
+++ src/components/Section.js
@@ -0,0 +1,13 @@
+export default class Section {
+  constructor({ items, renderer }, containerSelector) {
+    this._renderer = renderer;
+    this._items = items;
+    this._container = document.querySelector(containerSelector);
+  }
+  addItem(element) {
+    this._container.append(element);
+  }
+  renderer() {
+    this._items.forEach((item) => this._renderer(item));
+  }
+}
